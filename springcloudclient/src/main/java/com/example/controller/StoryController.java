6dc989ad78d567cd9bb9c38dd8d59bd7c715951e
MCvolcano <2415575070@qq.com>
2018-01-11 09:34:23 +0800

    首次提交

---
+++ springcloudclient/src/main/java/com/example/controller/StoryController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+
+/**
+ * @Author：马灿
+ * @Description:
+ * @Date: 上午 9:28 2018-01-11
+ */
+public class StoryController {
+
+    public static void main(String[] args) {
+        System.out.println("测试用");
+    }
+}

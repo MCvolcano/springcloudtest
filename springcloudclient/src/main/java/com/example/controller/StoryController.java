52aee3ff240b1cd74c0b0351a70ad6d58ed4ceec
MCvolcano <2415575070@qq.com>
2018-01-11 09:42:42 +0800

    分支2 提交

--- springcloudclient/src/main/java/com/example/controller/StoryController.java
+++ springcloudclient/src/main/java/com/example/controller/StoryController.java
@@ -14,6 +14,16 @@ public class StoryController {
 
     public String name ;
 
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
